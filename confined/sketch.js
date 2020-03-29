f4a3b1738724f9766296a78bdcb42001147d3252
Nicolas d'Alessandro <nicolas@hovertone.com>
2020-03-29 22:12:18 +0200

    Added levels and win/lose.

--- confined/sketch.js
+++ confined/sketch.js
@@ -5,8 +5,8 @@ class Particle {
     this.x = random( 50, width-50 );
     this.y = random( 50, height-50 );
     
-    this.xSpeed = random( -1.2, 1.2 );
-    this.ySpeed = random( -1.2, 1.2 );
+    this.xSpeed = random( -0.7, 0.7 );
+    this.ySpeed = random( -0.7, 0.7 );
     
     this.d = random( 20, 30 );
     
@@ -80,21 +80,15 @@ class Particle {
 let particles = []; // particles
 let washImg; // wash hands image
 
-// let bgSound; // background sound
-
-/*
-function preload() {
-  
-  bgSound = loadSound( 'https://kyojindo.github.io/confined/soundtrack.mp3' );
-}
-*/
+let nOfContam = 0; // # contam
+let hasWon = false; // has user won?
+let hasLost = false; // has user lost?
+let particuleSpeed = 0.7;
 
 function setup() {
   
   createCanvas( windowWidth, windowHeight );
   washImg = loadImage( 'wash.png' ); // wash picto
-
-  // bgSound.loop(); // put the sound in loop mode
   
   for( let i=0; i<width/14; i++ ) {
     
@@ -104,18 +98,74 @@ function setup() {
 
 function draw() {
   
+  nOfContam = 0;
   background( '#ffffff' );
-  
-  for( let i=0; i<particles.length; i++ ) {
-    
-    particles[i].update();
-    particles[i].drawParticle();
-    particles[i].drawJoints( particles );
-    
-    if( mouseIsPressed ) {
+
+  if( !hasWon && !hasLost ) {
+
+    for( let i=0; i<particles.length; i++ ) {
       
-      particles[i].isTouched( mouseX, mouseY );
-      image( washImg, mouseX-25, mouseY-25, 50, 50 );
+      particles[i].update();
+      particles[i].drawParticle();
+      particles[i].drawJoints( particles );
+      
+      if( mouseIsPressed ) {
+        
+        particles[i].isTouched( mouseX, mouseY );
+        image( washImg, mouseX-25, mouseY-25, 50, 50 );
+      }
+
+      if( particles[i].contam == true ) nOfContam++;
+    }
+
+    if( nOfContam >= particles.length ) { hasLost = true; hasWon = false; }
+    if( nOfContam <= 0 ) { hasWon = true; hasLost = false; particuleSpeed += 0.2; }
+
+    textSize( 24 );
+    noStroke(); fill( 'rgba( 255, 0, 0, 0.6 )' );
+    text( nOfContam+' / '+particles.length, 20, 40 );
+
+  } else {
+
+    if( hasWon ) {
+
+      textSize( 100 );
+      noStroke(); fill( 'rgba( 255, 0, 0, 0.9 )' );
+      text( 'BRAVO !', width/2-210, height/2 );
+
+      if( keyIsPressed ) {
+
+          hasWon = false; // has user won?
+          hasLost = false; // has user lost?
+
+          for( let i=0; i<particles.length; i++ ) {
+
+            if( random( 0, 100 ) < 70 ) particles[i].contam = false;
+            else particles[i].contam = true; // random contamination
+
+            particles[i].xSpeed = random( -particuleSpeed, particuleSpeed );
+            particles[i].ySpeed = random( -particuleSpeed, particuleSpeed );
+          }
+      }
+    }
+
+    if( hasLost ) {
+
+      textSize( 100 );
+      noStroke(); fill( 'rgba( 255, 0, 0, 0.9 )' );
+      text( 'GAME OVER !', width/2-340, height/2 );
+
+      if( keyIsPressed ) {
+
+          hasWon = false; // has user won?
+          hasLost = false; // has user lost?
+
+          for( let i=0; i<particles.length; i++ ) {
+
+            if( random( 0, 100 ) < 70 ) particles[i].contam = false;
+            else particles[i].contam = true; // random contamination
+          }
+      }
     }
   }
 }
