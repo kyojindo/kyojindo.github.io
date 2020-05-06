888be826c3addf97627ffe56559ce1894a0ef57f
Nicolas d'Alessandro <nicolas@hovertone.com>
2020-05-06 17:53:53 +0200

    Try geoloc for p5.js.

--- geofun/sketch.js
+++ geofun/sketch.js
@@ -1,15 +1,41 @@
+let urlParams;
+let spotLgt = 0.0;
+let spotLat = 0.0;
+let geoFence;
+
 function setup() {
+
+  urlParams = getURLParams(); // get params
+
+  spotLgt = urlParams.lgt; // get lgt from url
+  spotLat = urlParams.lat; // get lat from url
+
+  geoFence = new geoFenceCircle( spotLgt, spotLat, 0.05 );
 
   createCanvas( windowWidth, windowHeight );
 }
 
 function draw() {
 
-  clear(); // clear & bg
+  clear(); noStroke();
   background( 30, 30, 30 );
 
-  noStroke(); fill( 255, 255, 255 );
+  textAlign( CENTER );
+  let geoText = "";
+
+  if( geoFence.insideFence ) {
+
+    geoText = "Tu es chez toi!";
+    fill( 255, 0, 0 );
+
+  } else {
+
+    geoText = "Tu n'es pas chez toi!";
+    fill( 255, 255, 255 );
+  }
+
   ellipse( width/2, height/2, 100, 100 );
+  text( geoText, width/2, height/2+80 );
 }
 
 function windowResized() {
